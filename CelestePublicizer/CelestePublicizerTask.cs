5ec0e6d51b6593a5c99cecedf84773c080041135
psyGamer <psygamer21@gmail.com>
2023-12-29 19:55:10 +0100

    Initial commit

---
+++ CelestePublicizer/CelestePublicizerTask.cs
@@ -0,0 +1,52 @@
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+
+namespace CelestePublicizer
+{
+    /// <summary>
+    ///     MSBuild task for CelestePublicizer.
+    /// </summary>
+    public class CelestePublicizerTask
+        : Task
+    {
+        /// <summary>
+        ///     The task's input file.
+        /// </summary>
+        [Required]
+        public ITaskItem InputFile { get; set; }
+
+        /// <summary>
+        ///     Receives the input file's content.
+        /// </summary>
+        [Output]
+        public string Content { get; set; }
+
+        /// <summary>
+        ///     Execute the task.
+        /// </summary>
+        /// <returns>
+        ///     <c>true</c>, if the task executed succesfully; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Execute()
+        {
+            FileInfo inputFile = new FileInfo(
+                InputFile.GetMetadata("FullPath")
+            );
+            if (!inputFile.Exists)
+            {
+                Log.LogError($"Input file '{inputFile.FullName}' not found.");
+
+                return false;
+            }
+
+            using (StreamReader reader = inputFile.OpenText())
+            {
+                Content = reader.ReadToEnd();
+            }
+
+            return true;
+        }
+    }
+}
