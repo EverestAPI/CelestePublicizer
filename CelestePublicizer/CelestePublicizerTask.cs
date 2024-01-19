afac090bd6bf025f04838be1564e854d9fdbb953
psyGamer <psygamer21@gmail.com>
2024-01-19 23:10:05 +0100

    Setup local package testing

--- CelestePublicizer/CelestePublicizerTask.cs
+++ CelestePublicizer/CelestePublicizerTask.cs
@@ -1,52 +1,102 @@
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
 
-namespace CelestePublicizer
-{
-    /// <summary>
-    ///     MSBuild task for CelestePublicizer.
-    /// </summary>
-    public class CelestePublicizerTask
-        : Task
+namespace CelestePublicizer;
+
+public class PublicizeTask : Task {
+    [Required]
+    public string IntermediateOutputPath { get; set; }
+
+    [Required]
+    public ITaskItem[] ReferencePath { get; set; }
+    
+    [Required]
+    public ITaskItem[] PackageReference { get; set; }
+
+    public override bool Execute()
     {
-        /// <summary>
-        ///     The task's input file.
-        /// </summary>
-        [Required]
-        public ITaskItem InputFile { get; set; }
-
-        /// <summary>
-        ///     Receives the input file's content.
-        /// </summary>
-        [Output]
-        public string Content { get; set; }
-
-        /// <summary>
-        ///     Execute the task.
-        /// </summary>
-        /// <returns>
-        ///     <c>true</c>, if the task executed succesfully; otherwise, <c>false</c>.
-        /// </returns>
-        public override bool Execute()
-        {
-            FileInfo inputFile = new FileInfo(
-                InputFile.GetMetadata("FullPath")
-            );
-            if (!inputFile.Exists)
+        var outputDirectory = Path.Combine(IntermediateOutputPath, "celeste-publicized");
+        Directory.CreateDirectory(outputDirectory);
+        
+        var referencesToPublicize = ReferencePath
+            .Where(x => x.HasMetadata("MaskAssembly"));
+        var packagesToPublicize = PackageReference
+            .Where(x => x.HasMetadata("MaskAssembly"));
+        
+        var toPublicize = referencesToPublicize.Concat(packagesToPublicize);
+        
+        foreach (var taskItem in toPublicize) {
+            var assemblyPath = taskItem.GetMetadata("FullPath");
+            var maskPath = taskItem.GetMetadata("MaskAssembly");
+            
+            var hash = ComputeHash(File.ReadAllBytes(assemblyPath), maskPath);
+            
+            var publicizedAssemblyPath = Path.Combine(outputDirectory, Path.GetFileName(assemblyPath));
+            var hashPath = publicizedAssemblyPath + ".md5";
+            
+            if (File.Exists(hashPath) && File.ReadAllText(hashPath) == hash)
             {
-                Log.LogError($"Input file '{inputFile.FullName}' not found.");
-
-                return false;
+                // Log.LogMessage($"{fileName} was already publicized, skipping");
+                continue;
             }
+        }
+        
+        return true;
+    }
+    
+    // Adapted from https://github.com/BepInEx/BepInEx.AssemblyPublicizer/blob/master/BepInEx.AssemblyPublicizer.MSBuild/PublicizeTask.cs#L132-L168
+    private static string ComputeHash(byte[] bytes, string mask) {
+        static void Hash(ICryptoTransform hash, byte[] buffer) {
+            hash.TransformBlock(buffer, 0, buffer.Length, buffer, 0);
+        }
 
-            using (StreamReader reader = inputFile.OpenText())
-            {
-                Content = reader.ReadToEnd();
-            }
+        static void HashString(ICryptoTransform hash, string str) => Hash(hash, Encoding.UTF8.GetBytes(str));
+        static void HashBool(ICryptoTransform hash, bool value) => Hash(hash, BitConverter.GetBytes(value));
+        static void HashInt(ICryptoTransform hash, int value) => Hash(hash, BitConverter.GetBytes(value));
 
+        using var md5 = MD5.Create();
+
+        HashString(md5, typeof(PublicizeTask).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+        HashString(md5, mask);
+
+        md5.TransformFinalBlock(bytes, 0, bytes.Length);
+
+        return ByteArrayToString(md5.Hash);
+    }
+    
+    private static string ByteArrayToString(IReadOnlyCollection<byte> data) {
+        var builder = new StringBuilder(data.Count * 2);
+
+        foreach (var b in data) {
+            builder.AppendFormat("{0:x2}", b);
+        }
+
+        return builder.ToString();
+    }
+}
+
+// Taken from https://github.com/BepInEx/BepInEx.AssemblyPublicizer/blob/master/BepInEx.AssemblyPublicizer.MSBuild/Extensions.cs
+internal static class Extensions {
+    public static bool HasMetadata(this ITaskItem taskItem, string metadataName) {
+        var metadataNames = (ICollection<string>)taskItem.MetadataNames;
+        return metadataNames.Contains(metadataName);
+    }
+
+    public static bool TryGetMetadata(this ITaskItem taskItem, string metadataName, [NotNullWhen(true)] out string metadata) {
+        if (taskItem.HasMetadata(metadataName)) {
+            metadata = taskItem.GetMetadata(metadataName);
             return true;
         }
+
+        metadata = null;
+        return false;
     }
 }
---
+++ CelestePublicizer/IgnoreAccessChecksToAttribute.cs
@@ -0,0 +1,6 @@
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+internal sealed class IgnoresAccessChecksToAttribute : Attribute {
+    public IgnoresAccessChecksToAttribute(string assemblyName) { }
+}
